618d75388d52d3eb94b86251d4581bf6431005d6
Обора Дмитрий <obora_dmitriy@keepcode.ru>
2024-06-20 19:17:32 +0300

    начал делать блок почему мы на главной

---
+++ components/main/whyUs.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { t } = useI18n();
+</script>
+
+<template>
+<section class="whyUs">
+  <div class="whyUs__container">
+      <h2>{{$t('page.main.whyUs.title')}}</h2>
+      <div class="whyUs__grid">
+
+      </div>
+  </div>
+</section>
+</template>
+
+<style scoped lang="scss">
+.whyUs{
+  @apply py-16;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import RatesAndLocations from '~/components/main/RatesAndLocations.vue';
-
-const { t } = useI18n();
 </script>
 
 <template>
   <main-the-first/>
+  <main-why-us/>
   <main-rates-and-locations/>
   <main-the-o-s/>
   <main-the-question/>
